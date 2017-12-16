15436fb4c1c7165f217cf4751726dd7f2bc70d07
Ryan Possardt <rpossardt@gmail.com>
2017-12-16 10:14:33 -0500

    added socket.io

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
+const server      = require('http').createServer(app);
+const io          = require('socket.io')(server);
+
+let redditNamespace;
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
@@ -11,8 +15,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.listen(app.get('port'), () => {
-  console.log('Server started at http://localhost:${app.get("port")}/');
+redditNamespace = io.of('/newPosts');
+redditNamespace.on('connect', socket => {
+  redditNamespace.emit('hello', {message : 'Hello curious traveler!'});
+});
+
+server.listen(app.get('port'), () => {
+  console.log('Server started at http://localhost:' + app.get('port') + '/');
 });
 
 exports = module.exports = app;
